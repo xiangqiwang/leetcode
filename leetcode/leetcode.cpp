220a43040c50ed25669b1529c0ef6c9771a063d2
chess <296667528@qq.com>
2018-03-18 01:27:42 +0800

    new project

---
+++ leetcode/leetcode.cpp
@@ -0,0 +1,22 @@
+// leetcode.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution11
+{
+public:
+	int maxArea(vector<int>& height)
+	{
+		
+	}
+};
+int main()
+{
+    return 0;
+}
+
